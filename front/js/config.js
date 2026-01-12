5bbedd86792a95d29cddcaedb7f8a3e8e53b398f
Cesar Antonio mtz <xdc9125@gmail.com>
2026-01-12 10:54:18 -0600

    j

--- front/js/config.js
+++ front/js/config.js
@@ -12,7 +12,7 @@ const CONFIG = {
         // O dejar esta lógica para que funcione si sirven el front desde el mismo dominio (no es el caso aquí).
 
         // Poner aquí la URL de tu túnel (ej: https://mi-app.tunnelto.dev)
-        return 'https://exempt-enters-happened-flush.trycloudflare.com';
+        return 'https://covers-chi-patterns-trusted.trycloudflare.com';
     })()
 };
 
